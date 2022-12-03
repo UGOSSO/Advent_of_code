804cc81c5a5fd27962a01e0c46acefcfb638e5a3
Ugo Gosso <116841979+UGOSSO@users.noreply.github.com>
2022-12-03 07:13:59 +0100

    slight correction

--- (2020)_sixth_edition/d1.py
+++ (2020)_sixth_edition/d1.py
@@ -24,11 +24,19 @@ def expense_report_3(L) :
         output : the product between the three number in L which sum egal to 2020
     """
     
-    for i in L :
-        for j in L :
-            #2020-i-j is in L we have i in L, j in L, k = 2020-i-j in L and i+j+k = 2020 by definition so we have our three number 
-            if 2020-i-j in L :
-                return i*j*(2020-i-j)
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if i!=j:
+                if 2020-L[i]-L[j] in L :
+                    ind = [ind for ind, x in enumerate(L) if x == 2020-L[i]-L[j]]
+                    
+                    if i in ind : lim = 1
+                    else : lim = 0
+                    
+                    if j in ind : lim += 1
+                    
+                    if len(ind)> lim:
+                        print( (2020-L[i]-L[j])*L[i]*L[j])
 
 # print(expense_report_2(list_of_values)) #-> Part I
 # print(expense_report_3(list_of_values)) #-> Part II
